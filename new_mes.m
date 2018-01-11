e99e4bf1b159228a6628f22c2286e160510d6731
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2018-01-11 02:54:12 +0100

    Chyba nawet coś działa

--- new_mes.m
+++ new_mes.m
@@ -56,7 +56,7 @@ triplot(TRI, W(:, 1), W(:, 2), 'Color', [0.5 0.5 0.5]), axis equal, axis([0 dim(
 rectangle('Position', [s_pos s_dim], 'LineWidth', 2, 'EdgeColor', 'k'), hold on;
 rectangle('Position', [s_pos+s_d s_dim-2*s_d], 'LineWidth', 2, 'EdgeColor', 'k'), hold on;
 plot([dim(1) dim(1)], [0 dim(2)], 'Color', 'r', 'LineWidth', 2), hold on;
-plot(s_bor(2, 1:2), s_bor(2, 1:2)+[0 -s_d*2+s_dim(2)], 'Color', 'r', 'LineWidth', 2), hold on;
+plot(s_bor(2, 1:2), s_bor(2, 1:2)+[0 -s_d*2+s_dim(2)], 'Color', 'b', 'LineWidth', 2), hold on;
 
 % shield area
 [s_pol_x, s_pol_y] = polybool('subtraction', s_pol(1, :), s_pol(2, :), s_pol(3, :), s_pol(4, :));
@@ -75,7 +75,48 @@ s_tri = s_tri(1:t, :); % triangles inside shield walls/borders
 H = zeros(m);
 for i = 1:1:z
     t = TRI(i,:);
+    if ismember(t, s_tri, 'rows') == 1
+        e_tmp = E(2);
+    else
+        e_tmp = E(1);
+    end
     p = [ W(t(1), :); W(t(2), :); W(t(3), :) ];
-    he = getHe(p, E(1));
+    he = getHe(p, e_tmp);
     H(TRI(i, 1:3), TRI(i, 1:3)) = H(TRI(i, 1:3), TRI(i, 1:3)) + he;
 end
+
+source_points_v1 = zeros(30, 1);
+source_points_v2 = zeros(30, 1);
+t1 = 1;
+t2 = 1;
+for i = 1:1:m
+    tmp = W(i, :);
+    if tmp(1) == s_bor(2, 1) && tmp(2) >= s_bor(2, 1) && tmp(2) <= (s_bor(2, 1) - s_d*2 + s_dim(2))
+        source_points_v1(t1) = i;
+        t1 = t1 + 1;
+    elseif tmp(1) == dim(1)
+        source_points_v2(t2) = i;
+        t2 = t2 + 1;
+    end
+end
+source_points_v1 = source_points_v1(1:(t1 - 1));
+source_points_v2 = source_points_v2(1:(t2 - 1));
+source_points_all = [source_points_v1; source_points_v2];
+
+VV = zeros(m, 1);
+
+VV(source_points_v1) = V(1);
+VV(source_points_v2) = V(2);
+
+for i = 1:1:size(source_points_all, 1)
+    source_points = source_points_all(i);
+    H(source_points, :) = 0;
+    H(source_points, source_points) = 1;
+end
+
+B = H \ VV;
+figure(2);
+trimesh(TRI , W(:,1) , W(:,2),B);
+
+figure(3)
+contour(xs, ys, reshape(B, y, x), 20);
